2852f82fc46bdc5435e0deb26404fb52c813d11c
Dominic Myers <annoyingmouse@gmail.com>
2025-07-14 14:21:11 +0100

    🔧 Add .env, node_modules/, .idea/*, package-lock.json, and database.sqlite3 to .gitignore 🔧 Add config/config.json for development, test, and production environments 🔧 Add Sequelize model for User with id, username, email, password, createdAt, and updatedAt 🔧 Add Sequelize model for User with username, email, and password 🔧 Update package.json with dependencies and devDependencies 🔧 Add routes for user registration, login, and authentication middleware 🔧 Add validation schema for user registration and login inputs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Happy developing ✨')
+const express = require('express');
+
+//Import and initialise the environment variables - stored in .dotenv file in project root
+require('dotenv').config();
+
+//Import Routes
+const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protected');
+
+//Initialise Express
+const app = express();
+
+//Set up parser so we can read JSON info from the request
+app.use(express.json());
+
+//Set up API routes
+app.use('/api/user', authRoutes);
+app.use('/api/protected', protectedRoutes);
+
+//404 Error routes as a catch-all
+app.use((req, res, next)=>{
+  res.status(404).send("Error 404 - Resource not found");
+});
+
+//Set the default port to serve app on
+const port = process.env.PORT || 3000;
+
+app.listen(port, console.log(`App running on port: ${port}`));
