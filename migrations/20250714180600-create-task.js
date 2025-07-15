0453340812b252c9de63e1efb60dd7e9a1a7a1a9
Dominic Myers <annoyingmouse@gmail.com>
2025-07-15 07:04:18 +0100

    ✨ (migrations): create migration file to add Tasks table to the database 📝 (models): add Task model file to define the structure of the Task entity in the application

---
+++ migrations/20250714180600-create-task.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Tasks", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dueDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL"
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable("Tasks");
+  },
+};
